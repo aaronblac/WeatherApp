85eb486989dd8e9d5e5047d7a624a408ea7278bb
aBlack <aaronblac@gmail.com>
2020-12-30 14:48:27 -1000

    search any citys weather

--- app.js
+++ app.js
@@ -1,21 +1,39 @@
 const express = require('express');
 const https = require('https');
+const bodyParser = require('body-parser');
 
 const app = express();
-const url = "https://api.openweathermap.org/data/2.5/weather?q=Lihue&appid=49e99c8e9cdeb289f185a7bd0f754e4f&units=imperial";
+
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get('/', (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+})
+
+app.post('/', (req, res) => {
+  const query = req.body.cityName;
+  const apiKey = "49e99c8e9cdeb289f185a7bd0f754e4f";
+  const units = "imperial";
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=imperial";
+
   https.get( url, (response) => {
     console.log(response.statusCode);
 
     response.on('data', (data) => {
     const weatherData =  JSON.parse(data);
-      console.log(weatherData);
+    const weatherDescription = weatherData.weather[0].description;
+    const temp = weatherData.main.temp;
+    const icon = weatherData.weather[0].icon;
+    const imageURL = 'http://openweathermap.org/img/wn/' + icon + '@2x.png';
+
+    res.write("<h1>The temperature in " + query + " is " + temp + " degrees Farenheight</h1>");
+    res.write("<p>The weather is currently " + weatherDescription + ".</p>");
+    res.write("<img src = " + imageURL + ">");
+    res.send();
     })
   });
-  console.log('server is up and running');
+
 })
-
-
 
 
 app.listen(3000, () =>{
